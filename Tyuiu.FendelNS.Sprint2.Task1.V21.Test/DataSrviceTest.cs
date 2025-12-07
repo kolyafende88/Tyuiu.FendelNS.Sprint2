941ae8dc2ae36d0cda8fd39cca4c5f56116ced8f
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-08 00:42:36 +0500

    Task1

--- Tyuiu.FendelNS.Sprint2.Task1.V21.Test/DataSrviceTest.cs
+++ Tyuiu.FendelNS.Sprint2.Task1.V21.Test/DataSrviceTest.cs
@@ -2,49 +2,21 @@
 
 namespace Tyuiu.FendelNS.Sprint2.Task1.V21.Test
 {
-    internal class Program
+    [TestClass]
+    public class DataServiceTest
     {
-        static void Main(string[] args)
+        [TestMethod]
+        public void ValidGetLogicOperations()
         {
             DataService ds = new DataService();
-
             int a = 696;
             int b = 987;
             int c = 696;
             int d = 155;
-
             bool[] res = new bool[6];
             res = ds.GetLogicOperations(a, b, c, d);
-
-            Console.Title = "Спринт #2 | Выполнил: Фендель Н. С. | СМАРТб-24-1";
-            Console.WriteLine("************************************************************************************************************************");
-            Console.WriteLine("* Спринт #2                                                                                                            *");
-            Console.WriteLine("* Тема: Логические операции                                                                                            *");
-            Console.WriteLine("* Задание #1                                                                                                           *");
-            Console.WriteLine("* Вариант #21                                                                                                          *");
-            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич| СМАРТб-24-1                                                                     *");
-            Console.WriteLine("************************************************************************************************************************");
-            Console.WriteLine("* УСЛОВИЕ:                                                                                                             *");
-            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность можно чередовать,                 *");
-            Console.WriteLine("* но использовать один раз в выражении)                                                                                *");
-            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться),                        *");
-            Console.WriteLine("* а также арифметических выражений, которая вернет логическую последовательность(массив):                              *");
-            Console.WriteLine("* (False, False, False, False, True, False), при a = 696, b = 987, c = 696, d = 155                                    *");
-            Console.WriteLine("************************************************************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                     *");
-            Console.WriteLine("************************************************************************************************************************");
-            Console.WriteLine("a = " + a);
-            Console.WriteLine("b = " + b);
-            Console.WriteLine("c = " + c);
-            Console.WriteLine("d = " + d);
-            Console.WriteLine("************************************************************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                           *");
-            Console.WriteLine("************************************************************************************************************************");
-            for (int i = 0; i < 6; i++)
-            {
-                Console.WriteLine(res[i]);
-            }
-            Console.ReadKey();
+            bool[] wait = new bool[6] { false, false, false, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
