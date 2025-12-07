21459f985ebf1a37263e5b88518d339c117ba31c
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-08 00:38:57 +0500

    Task1

---
+++ Tyuiu.FendelNS.Sprint2.Task1.V21.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.FendelNS.Sprint2.Task1.V21.Lib
+{
+    public class DataService : ISprint2Task1V21
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a > b) | (c < d);
+            res[1] = (a + 5 > b) & (c < d);
+            res[2] = (a > b) || (c < d);
+            res[3] = (a + 5 > b) && (c < d);
+            res[4] = !res[0];
+            res[5] = (a > b) ^ (c < d);
+            return res;
+        }
+    }
+}
