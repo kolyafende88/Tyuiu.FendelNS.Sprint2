4a9914d71a231388c62630344543afb03263ea17
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-09 23:36:39 +0500

    Task6

---
+++ Tyuiu.FendelNS.Sprint2.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.FendelNS.Sprint2.Task6.V1.LIb;
+namespace Tyuiu.FendelNS.Sprint2.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void FindMonthDaysCount()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(31, ds.FindMonthDaysCount(1));
+            Assert.AreEqual(28, ds.FindMonthDaysCount(2));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(3));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(4));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(5));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(6));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(7));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(8));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(9));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(10));
+            Assert.AreEqual(30, ds.FindMonthDaysCount(11));
+            Assert.AreEqual(31, ds.FindMonthDaysCount(12));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindMonthDaysCount(-1);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindMonthDaysCount(13);
+            });
+        }
+    }
+}
---
+++ Tyuiu.FendelNS.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.FendelNS.Sprint2.Task6.V1.LIb;
+namespace Tyuiu.FendelNS.Sprint2.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Фендель Н. С. | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич | СМАРТБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int numMonth = Convert.ToInt32(Console.ReadLine());
+            int res;
+            if ((numMonth > 12) || (numMonth < 1))
+            {
+                res = 0;
+            }
+            else
+            {
+                res = ds.FindMonthDaysCount(numMonth);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"Количество дней в месяце: {res}");
+            Console.ReadKey();
+        }
+    }
+}
