5ce3b05a9cf6d9d723bdd1bd19ef3e40ca6f8325
Tyuiu.FendelNS <Tyuiu.FendelNS@outlook.com>
2025-12-09 23:26:49 +0500

    Task5

---
+++ Tyuiu.FendelNS.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.FendelNS.Sprint2.Task5.V3.Lib;
+namespace Tyuiu.FendelNS.Sprint2.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Фендель Н. С. | СМАРТБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Фендель Николай Сергеевич | СМАРТБ-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат. Составить программу, которая *");
+            Console.WriteLine("* в зависимости от порядкового номера дня недели (1, 2, …, 7) выводит на  *");
+            Console.WriteLine("* экран его название (понедельник, вторник, …, воскресенье).              *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер дня недели: ");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numDay < 1) || (numDay > 7))
+            {
+                res = "Введенно неверное значение!";
+            }
+            else
+            {
+                res = "Это день недели: " + ds.FindDayName(numDay);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
